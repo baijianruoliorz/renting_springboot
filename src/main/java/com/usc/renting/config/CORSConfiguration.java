f1262c7908a68a26a98aa15a363b1749e9c2939f
liqiqiorz <1099462011@qq.com>
2020-07-02 18:09:43 +0800

    修改一些小细节

--- src/main/java/com/usc/renting/config/CORSConfiguration.java
+++ src/main/java/com/usc/renting/config/CORSConfiguration.java
@@ -1,17 +1,21 @@
 package com.usc.renting.config;
-
+/*
+*  @author liqiqiorz
+*  @date 2020.04.07
+* */
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 @Configuration
 public class CORSConfiguration extends WebMvcConfigurerAdapter{
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 所有请求都允许跨域
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
-    }
+        @Override//配置在config包下
+            public void addCorsMappings(CorsRegistry registry) {
+                // 所有请求都允许跨域
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+
+
 }
